aeb27a41e092ee424cb4d1cb7544b13b6af6275b
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-09-17 14:21:38 +0200

    Animals data, schema and queries

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+('1', 'Agumon', 'Feb 3 2020', '0', 'Yes', '10.23'),
+('2', 'Gabumon', 'Nov 15 2018', '2', 'Yes', '8'),
+('3', 'Pikachu', 'Jan 7 2021', '1', 'No', '15.04'),
+('4', 'Devimon', 'May 12 2017', '5', 'Yes', '11');
