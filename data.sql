b61a8b56a6f4bccc02042cba546c99e5f73e042c
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-09-17 19:04:19 +0200

    Use of transaction and update table

--- data.sql
+++ data.sql
@@ -3,3 +3,14 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 ('2', 'Gabumon', 'Nov 15 2018', '2', 'Yes', '8'),
 ('3', 'Pikachu', 'Jan 7 2021', '1', 'No', '15.04'),
 ('4', 'Devimon', 'May 12 2017', '5', 'Yes', '11');
+
+-- Query and Update animals table
+
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+('5', 'Charmander', 'Feb 8 2020', '0', 'No', '11'),
+('6', 'Plantmon', 'Nov 15 2021', '2', 'Yes', '5.7'),
+('7', 'Squirtle', 'Apr 2 1993', '3', 'No', '12.13'),
+('8', 'Angemon', 'Jun 12 2005', '1', 'Yes', '45'),
+('9', 'Boarmon', '07 Jun 2005', '7', 'Yes', '20.4'),
+('10', 'Blossom', '13 Oct 1998', '3', 'Yes', '17'),
+('11', 'Ditto', '14 May 2022', '4', 'Yes', '22');
--- schema.sql
+++ schema.sql
@@ -8,3 +8,8 @@ CREATE TABLE animals (
 	neutered BOOLEAN,
 	weight_kg DECIMAL
 );
+
+-- Query and Update animals table
+
+ALTER TABLE animals
+ADD species VARCHAR(50);
